d15642177dad5929a545a734217abb0a9a797cac
Kenton Lam <kenton_lam@outlook.com>
2022-01-19 22:20:14 +1000

    Card.

---
+++ src/Card.svelte
@@ -0,0 +1,12 @@
+<img src="cards/card_diamonds_A.png" alt="card">
+
+<style>
+  img {
+    image-rendering: -moz-crisp-edges;
+    image-rendering: -o-crisp-edges;
+    image-rendering: pixelated;
+    -ms-interpolation-mode: nearest-neighbor;
+
+    width: 128px;
+  }
+</style>
--- src/Game.svelte
+++ src/Game.svelte
@@ -1,10 +1,34 @@
+<script lang="ts">
+import Card from "./Card.svelte";
+
+  let stacks: number[] = [1,2,3,4,5,6,7,8,9,10];
+</script>
 
 <div id="game">
-    Game
+  Solitaire
+  <div id="board">
+    {#each stacks as stack}
+      <div class="stack">
+        <Card></Card>
+      </div>
+    {/each}
+  </div>
 </div>
 
 <style>
-    #game {
-        background-color: red;
-    }
+  #game {
+    background-color: red;
+  }
+
+  #board {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .stack {
+    /* margin: 10px; */
+    background-color: green;
+  }
 </style>
