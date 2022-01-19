c36324d05a3adc704bac19208b5a1a56809e399f
Kenton Lam <kenton_lam@outlook.com>
2022-01-19 21:54:09 +1000

    Initial commit.

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let name: string;
+
+	import Game from "./Game.svelte";
+</script>
+
+<main>
+	<Game></Game>
+</main>
+
+<style>
+
+</style>
---
+++ src/Game.svelte
@@ -0,0 +1,10 @@
+
+<div id="game">
+    Game
+</div>
+
+<style>
+    #game {
+        background-color: red;
+    }
+</style>
